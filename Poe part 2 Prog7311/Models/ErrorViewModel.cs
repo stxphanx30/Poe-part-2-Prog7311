b00ad099764976a91cfa145ee9abdb3e62df5639
stxphanx30 <ST10158209@vcconnect.edu.za>
2025-05-11 07:47:16 +0200

    Add project files.

---
+++ Poe part 2 Prog7311/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Poe_part_2_Prog7311.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
